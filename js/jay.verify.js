abc7fb66b86e1932033fe6a19422694522982eb0
Alex Jones <alex@jonesnet.tk>
2014-11-26 02:29:24 -0800

    worring about cross domain, calling it for the night

--- js/jay.verify.js
+++ js/jay.verify.js
@@ -1,8 +1,34 @@
 var Jay = (function(Jay, $, undefined) {
+	Jay.verify = {};
 
+	//ugly but necesarry side function, ill put it somewhere else later, or not...
+	function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+	Jay.verify.alive = function() {
+
+
+	}
 	
+	Jay.verify.run = function() {
+		var host = getParameterByName('host');
+		var hop = getParameterByName('hop');
 
+		// get the metadata sent to us from the GET data
+		for(var a=0;a<Jay.verify.filelist.length;a++)
+		{
+			$("#loadframe").attr("src", ("http://"+host+Jay.verify.filelist[a]));
+			$("#loadframe").load(function() {
+				alert(this.sandbox.toString())
+			})
+		}
+
+	}
 	
-
+	Jay.verify.filelist = ["/index.php"]; // many more files here later
 	return Jay;
 }(Jay || {}, jQuery));
