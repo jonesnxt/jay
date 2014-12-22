5a34f270c9d107a73d2dfc0728e224e5a1181539
Alex Jones <alex@jonesnet.tk>
2014-12-21 19:19:31 -0800

    simple db, error catching stub

--- js/jay.db.js
+++ js/jay.db.js
@@ -6,17 +6,34 @@ var Jay = (function(Jay, $, undefined) {
 
 	Jay.db.newTable = function(tablename, rows)
 	{
-		Jay.db.database[tablename] = {};
+		if(typeof(Jay.db.meta[tablename]) !== undefined) Jay.error.fatal("Failed to create table \"" + tablename +"\", already exists");
+
+		Jay.db.database[tablename] = array();
 		Jay.db.meta[tablename] = rows;
-		for (var a=0;a<rows.length;a++)
-		{
-			Jay.db.database[tablename][rows[a]] = array();
-		}
 	}
 
 	Jay.db.insert = function(tablename, data)
 	{
+		if(typeof(Jay.db.meta[tablename]) === undefined)
+			Jay.error.fatal("Table \"" + tablename + "\" doesn't exist");
+		//if(Jay.db.meta[tablename].length !== data.length) 
+		//	Jay.error.fatal("Table \""+tablename+"\" has " + Jay.db.meta[tablename].length+" rows, given "+data.length);
 
+		Jay.db.database[tablename].push(data);
+	}
+
+	Jay.db.select = function(tablename, index, value)
+	{
+		if(typeof(Jay.db.meta[tablename]) === -1)
+			Jay.error.fatal("Table \"" + tablename + "\" doesn't exist");
+		if($.inArray(index, Jay.db.meta[tablename] === false)
+			Jay.error.fatal("Table \"" + tablename + "\" doesn't have index \""+index+"\""); 
+
+		for(var a=0;a<Jay.db.database[tablename].length;a++)
+		{
+			if(Jay.db.database[tablename][a][index] == value) return Jay.db.database[tablename][a];
+		}
+		return false;
 	}
 
 	return Jay;
---
+++ js/jay.error.js
@@ -0,0 +1,21 @@
+var Jay = (function(Jay, $, undefined) {
+
+	Jay.error = {};
+
+	Jay.error.warning = function(message) {
+
+
+	}
+
+	Jay.error.fatal = function(message) {
+
+
+	}
+
+	Jay.error.info = function(message) {
+
+		
+	}
+
+	return Jay;
+}(Jay || {}, jQuery));
