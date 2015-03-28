b85758770c2ee8cb89bfa913c9c6da53104fef80
Alex Jones <alex@jonesnet.tk>
2015-03-28 14:37:29 -0700

    minified version

--- js/jay.js
+++ js/jay.js
@@ -158,6 +158,94 @@ var Jay = {};
 				Jay.queue(Jay.bestNodes[0], parameters, onSuccess, onFailure);
 			}
 		}
+		else if(requestMethod == Jay.requestMethods.validate)
+		{
+			var vld = [];
+			for(var a=0;a<3;a++)
+			{
+				Jay.queue(Jay.bestNodes[a], parameters, function(resp, status, xhr) {
+					vld.push(resp);
+					if(vld.length == 3)
+					{
+						// compare
+						if(Jay.objectCompare(vld[0], vld[1]))
+						{
+							onSuccess(vld[0], "success", xhr);
+						}
+						else if(Jay.objectCompare(vld[1], vld[2]))
+						{
+							onSuccess(vld[1], "success", xhr);
+						}
+						else if(Jay.objectCompare(vld[0], vld[2]))
+						{
+							onSuccess(vld[2], "success", xhr);
+						}
+						else
+						{
+							onFailure({"error":"Unable to Validate"}, "error", xhr);
+						}
+					}
+				});
+			}
+		}
+	}
+
+	Jay.objectCompare = function(o1, o2, params)
+	{
+		if(params === undefined)
+		{
+			// search for all things
+			o1.requestProcessingTime = 0;
+			o2.requestProcessingTime = 0;
+			alert(params);
+			return objectEquals(o1, o2);
+		}
+		else
+		{
+			// compare only the objects in params..
+			for(var b=0;b<params.length;b++)
+			{
+				if(params[b] in o1 && params[b] in o2)
+				{
+					if(typeof(o1[params[b]]) == Object)
+					{
+						if(JSON.stringify(o1[params[b]]) != JSON.stringify(o2[params[b]])) return false;
+					}
+					else
+					{
+						if(o1[params[b]] != o[params[b]]) return false;
+					}
+				}
+				else return false;
+		 	}
+			return true;
+
+		}
+	}
+
+	function objectEquals(x, y) {
+    'use strict';
+
+    if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+    // after this just checking type of one would be enough
+    if (x.constructor !== y.constructor) { return false; }
+    // if they are functions they should exactly refer to same one
+    if (x instanceof Function) { return x === y; }
+    if (x === y || x.valueOf() === y.valueOf()) { return true; }
+    if (Array.isArray(x) && x.length !== y.length) { return false; }
+
+    // if they are dates, they must had equal valueOf
+    if (x instanceof Date) { return false; }
+
+    // if they are strictly equal, they both need to be object at least
+    if (!(x instanceof Object)) { return false; }
+    if (!(y instanceof Object)) { return false; }
+
+    // recursive object equality check
+    var p = Object.keys(x);
+    return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) ?
+            p.every(function (i) { return objectEquals(x[i], y[i]); }) :
+            false;
 	}
 
 	Jay.types = {};
@@ -249,26 +337,6 @@ var Jay = {};
 		if(recip.length == 9) recip = recip.slice(0, 8);
 		while(recip.length < 8) recip = recip.concat(Jay.pad(1, 0));
 		return recip;
-	}
-
-	function byteArrayToBigInteger(byteArray, startIndex) {
-		var value = new BigInteger("0", 10);
-		var temp1, temp2;
-		for (var i = byteArray.length - 1; i >= 0; i--) {
-			temp1 = value.multiply(new BigInteger("256", 10));
-			temp2 = temp1.add(new BigInteger(byteArray[i].toString(10), 10));
-			value = temp2;
-		}
-
-		return value;
-	}
-
-	Jay.bytesToRs = function(bytes)
-	{
-		var accid = (byteArrayToBigInteger(bytes)).toString();
-		var rec = new NxtAddress();
-		rec.set(accid);
-		return rec.toString();
 	}
 
 	Jay.numberToBytes = function(num)
@@ -647,15 +715,215 @@ var Jay = {};
 		return data;
 	}
 
-$(document).ready(function() {
 
-document.write(Jay.sendMessage("NXT-RJU8-JSNR-H9J4-2KWKY","yes", Jay.addAppendage(Jay.appendages.publicKeyAnnouncement, "3a74c6848c01a0d7deb2eda978366ad29e366dc3fdea11868a06cc2677139213")));
-document.write("<br/>" + Jay.placeBidOrder("17435996739008103286", 100, 10.25));
 
-	//document.write((new Jay()).sendMoney("NXT-RJU8-JSNR-H9J4-2KWKY",100));
+var _hash = {
+		init: SHA256_init,
+		update: SHA256_write,
+		getBytes: SHA256_finalize
+	};
 
-	Jay.setRequestMethod(Jay.requestMethods.fastest);
-	Jay.request("getTime", {}, function(data, a, b) {
-	 	console.log(JSON.stringify(data));
-	});
-});
+function byteArrayToBigInteger(byteArray, startIndex) {
+		var value = new BigInteger("0", 10);
+		var temp1, temp2;
+		for (var i = byteArray.length - 1; i >= 0; i--) {
+			temp1 = value.multiply(new BigInteger("256", 10));
+			temp2 = temp1.add(new BigInteger(byteArray[i].toString(10), 10));
+			value = temp2;
+		}
+
+		return value;
+	}
+
+function simpleHash(message) {
+		_hash.init();
+		_hash.update(message);
+		return _hash.getBytes();
+	}
+
+var epochNum = 1385294400;
+function getPublicKey(secretPhrase)
+{
+	SHA256_init();
+	SHA256_write(converters.stringToByteArray(secretPhrase));
+	var ky = converters.byteArrayToHexString(curve25519.keygen(SHA256_finalize()).p);
+
+	return converters.hexStringToByteArray(ky);
+}
+
+function toByteArray(long) {
+    // we want to represent the input as a 8-bytes array
+    var byteArray = [0, 0, 0, 0];
+
+    for ( var index = 0; index < byteArray.length; index ++ ) {
+        var byte = long & 0xff;
+        byteArray [ index ] = byte;
+        long = (long - byte) / 256 ;
+    }
+
+    return byteArray;
+};
+
+	function toIntVal(byteArray) {
+	    // we want to represent the input as a 8-bytes array
+	    var intval = 0;
+
+	    for ( var index = 0; index < byteArray.length; index ++ ) {
+	    	var byt = byteArray[index] & 0xFF;
+	    	var value = byt * Math.pow(256, index);
+	    	intval += value;
+	    }
+
+	    return intval;
+	};
+
+	Jay.signBytes = function(message, secretPhrase) {
+		var messageBytes = message;
+		var secretPhraseBytes = converters.stringToByteArray(secretPhrase);
+
+		var digest = simpleHash(secretPhraseBytes);
+		var s = curve25519.keygen(digest).s;
+
+		var m = simpleHash(messageBytes);
+		_hash.init();
+		_hash.update(m);
+		_hash.update(s);
+		var x = _hash.getBytes();
+
+		var y = curve25519.keygen(x).p;
+
+		_hash.init();
+		_hash.update(m);
+		_hash.update(y);
+		var h = _hash.getBytes();
+
+		var v = curve25519.sign(h, x, s);
+
+
+		return v.concat(h);
+	}
+
+	function areByteArraysEqual(bytes1, bytes2) {
+		if (bytes1.length !== bytes2.length)
+			return false;
+
+		for (var i = 0; i < bytes1.length; ++i) {
+			if (bytes1[i] !== bytes2[i])
+				return false;
+		}
+
+		return true;
+	}
+
+	Jay.verifyBytes = function(signature, message, publicKey) {
+		var signatureBytes = signature;
+		var messageBytes = message;
+		var publicKeyBytes = publicKey;
+		var v = signatureBytes.slice(0, 32);
+		var h = signatureBytes.slice(32);
+		var y = curve25519.verify(v, h, publicKeyBytes);
+
+		var m = simpleHash(messageBytes);
+
+		_hash.init();
+		_hash.update(m);
+		_hash.update(y);
+		var h2 = _hash.getBytes();
+
+		return areByteArraysEqual(h, h2);
+	}
+
+	Jay.createToken = function(websiteString, secretPhrase)
+	{
+		//alert(converters.stringToHexString(websiteString));
+		var hexwebsite = converters.stringToHexString(websiteString);
+        var website = converters.hexStringToByteArray(hexwebsite);
+        var data = [];
+        data = website.concat(getPublicKey(secretPhrase));
+        var unix = Math.round(+new Date()/1000);
+        var timestamp = unix-epochNum;
+        var timestamparray = toByteArray(timestamp);
+        data = data.concat(timestamparray);
+
+        var token = [];
+        token = getPublicKey(secretPhrase).concat(timestamparray);
+
+        var sig = Jay.signBytes(data, secretPhrase);
+
+        token = token.concat(sig);
+        var buf = "";
+
+        for (var ptr = 0; ptr < 100; ptr += 5) {
+
+        	var nbr = [];
+        	nbr[0] = token[ptr] & 0xFF;
+        	nbr[1] = token[ptr+1] & 0xFF;
+        	nbr[2] = token[ptr+2] & 0xFF;
+        	nbr[3] = token[ptr+3] & 0xFF;
+        	nbr[4] = token[ptr+4] & 0xFF;
+        	var number = byteArrayToBigInteger(nbr);
+
+            if (number < 32) {
+                buf+="0000000";
+            } else if (number < 1024) {
+                buf+="000000";
+            } else if (number < 32768) {
+                buf+="00000";
+            } else if (number < 1048576) {
+                buf+="0000";
+            } else if (number < 33554432) {
+                buf+="000";
+            } else if (number < 1073741824) {
+                buf+="00";
+            } else if (number < 34359738368) {
+                buf+="0";
+            }
+            buf +=number.toString(32);
+
+        }
+        return buf;
+
+    }
+
+	Jay.parseToken = function(tokenString, website)
+	{
+ 		var websiteBytes = converters.stringToByteArray(website);
+        var tokenBytes = [];
+        var i = 0;
+        var j = 0;
+
+        for (; i < tokenString.length; i += 8, j += 5) {
+
+        	var number = new BigInteger(tokenString.substring(i, i+8), 32);
+            var part = converters.hexStringToByteArray(number.toRadix(16));
+
+            tokenBytes[j] = part[4];
+            tokenBytes[j + 1] = part[3];
+            tokenBytes[j + 2] = part[2];
+            tokenBytes[j + 3] = part[1];
+            tokenBytes[j + 4] = part[0];
+
+        }
+
+        if (i != 160) {
+            new Error("tokenString parsed to invalid size");
+        }
+        var publicKey = [];
+        publicKey = tokenBytes.slice(0, 32);
+        var timebytes = [tokenBytes[32], tokenBytes[33], tokenBytes[34], tokenBytes[35]];
+
+        var timestamp = toIntVal(timebytes);
+        var signature = tokenBytes.slice(36, 100);
+
+        var data = websiteBytes.concat(tokenBytes.slice(0, 36));
+       	
+        var isValid = Jay.verifyBytes(signature, data, publicKey);
+
+        var ret = {};
+        ret.isValid = isValid;
+        ret.timestamp = timestamp;
+        ret.publicKey = converters.byteArrayToHexString(publicKey);
+
+        return ret;
+
+	}
